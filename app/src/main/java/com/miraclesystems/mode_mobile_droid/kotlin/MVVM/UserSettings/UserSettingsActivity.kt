df3df504398fb9db3270eca789b220b371a7bc61
yada schneider <yada.schneider@miraclesystems.net>
2020-08-20 09:06:58 -0700

    created user settings package

---
+++ app/src/main/java/com/miraclesystems/mode_mobile_droid/kotlin/MVVM/UserSettings/UserSettingsActivity.kt
@@ -0,0 +1,12 @@
+package com.miraclesystems.mode_mobile_droid.kotlin.MVVM.UserSettings
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.miraclesystems.mode_mobile_droid.R
+
+class UserSettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_settings)
+    }
+}
